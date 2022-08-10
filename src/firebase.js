7090c3787c7b8b0561d9241800bcb4d22a4e0ea4
celociousalltrin <celociousalltrinmf@gmail.com>
2022-08-10 22:34:19 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDjYw_ykPwFEoSJX6ZuPRBvW3KGstvfFoU",
+    authDomain: "video-streaming-9149d.firebaseapp.com",
+    projectId: "video-streaming-9149d",
+    storageBucket: "video-streaming-9149d.appspot.com",
+    messagingSenderId: "116670319510",
+    appId: "1:116670319510:web:de01c81e5c82870adcabb7"
+  };
