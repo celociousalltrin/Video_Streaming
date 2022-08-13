527fa0562f19da7870cf1680b5136d7911bc655a
celociousalltrin <celociousalltrinmf@gmail.com>
2022-08-13 11:34:06 +0530

    Navbar completed

--- src/Component/Nav/Navbar.js
+++ src/Component/Nav/Navbar.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import "./Navbar.css";
 import logo from "../../Assets/Netflix-Logo.png";
-import avatar from "../../Assets/Netflix-avatar.png"
-
+import avatar from "../../Assets/Netflix-avatar.png";
+import {useState,useEffect} from "react";
 
 const Navbar = () => {
+  const [show,setShow] = useState(false)
+
+  const transitionNavbar = ()=>{
+    if(window.scrollY > 100){
+      setShow(true)
+    } else{
+      setShow(false)
+    }
+  };
+
+ useEffect(()=>{
+   window.addEventListener("scroll",transitionNavbar);
+
+   return (()=>window.removeEventListener("scroll",transitionNavbar));
+ },[]) 
+
+
   return (
-    <nav className="Navbar">
+    <nav className={`Navbar ${show && `Navbar__black`}`}>
         <div className="Navbar__contents">
           <img 
              src={logo}
