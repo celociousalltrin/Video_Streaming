980cfa90ca3b444681933100b1adaa994556e3d8
celociousalltrin <celociousalltrinmf@gmail.com>
2022-08-15 00:08:46 +0530

    Login Success

--- src/Component/SignInContent/SignInContent.js
+++ src/Component/SignInContent/SignInContent.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import "./SignInContent.css";
+import { useState } from 'react';
+import {createUserWithEmailAndPassword,signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../../firebase";
 
 const SignInContent = () => {
+  const [mail,setMail] = useState("")
+  const [password,setPassword] = useState("")
+
+  const register = ((e)=>{
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth,mail,password)
+        .then((authUser)=>{
+          console.log(authUser)
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+  })
+
+  const signIn = ((e)=>{
+    e.preventDefault()
+    signInWithEmailAndPassword(auth,mail,password)
+        .then((authUser)=>{
+          console.log(authUser)
+        })
+        .catch((err)=>{
+          console.log(err)
+        })
+  })
+
   return (
     <div className='SignInContent'>
       <form>
         <h1>Sign In</h1>
-        <input placeholder='Email' type='email' />
-        <input placeholder='password' type="password"/>
-        <button type='submit'>Sign In</button>
+
+        <input value={mail}
+        placeholder='Email' type='email' 
+        onChange={(e)=>setMail(e.target.value)}/>
+
+        <input value={password}
+        placeholder='password' type="password" 
+        onChange={(e)=>setPassword(e.target.value)}/>
+
+        <button type='submit' onClick={signIn}>Sign In</button>
         <h4>
          <span className='SignInContent__greybutton'>New to Netfllix?</span>
-         <span className='SignInContent__link'>Sign up Now</span>
+         <span className='SignInContent__link' onClick={register}>Sign up Now</span>
         </h4>
         </form>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import {initializeApp} from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {getFirestore} from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyDjYw_ykPwFEoSJX6ZuPRBvW3KGstvfFoU",
     authDomain: "video-streaming-9149d.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
     messagingSenderId: "116670319510",
     appId: "1:116670319510:web:de01c81e5c82870adcabb7"
   };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const db = getFirestore()
+
+  export {app, auth, db};
