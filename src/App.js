08f3f3ab162eeef9c5ddeae97050728d22287ae4
celociousalltrin <celociousalltrinmf@gmail.com>
2022-08-15 19:54:38 +0530

    Redux with login state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from "../src/Pages/HomeScreen/HomeScreen";
 import "./App.css";
 import ProfileScreen from './Pages/ProfileScreen/ProfileScreen';
 import LoginScreen from './Pages/LoginScreen/LoginScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 const App = () => {
-  const user = null;
+  const user = useSelector(selectUser);
+  console.log(user)
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    const unSubscribe = onAuthStateChanged(auth,(userAuth)=>{
+      if(userAuth){
+        dispatch(login({
+          uid:userAuth.uid,
+          email:userAuth.email,
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+    return unSubscribe;
+  },[]);
+
+
   return (
     <div className='app'>
       <Router>
@@ -24,4 +47,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
