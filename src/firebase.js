65ce1ced7e99e780c1d4bf4b402206c6f7b9d386
Celocious Alltrin MF <109226717+celociousalltrin@users.noreply.github.com>
2022-10-02 15:18:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,1 @@
-import {initializeApp} from "firebase/app";
-import { getAuth } from "firebase/auth";
-import {getFirestore} from "firebase/firestore"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDjYw_ykPwFEoSJX6ZuPRBvW3KGstvfFoU",
-    authDomain: "video-streaming-9149d.firebaseapp.com",
-    projectId: "video-streaming-9149d",
-    storageBucket: "video-streaming-9149d.appspot.com",
-    messagingSenderId: "116670319510",
-    appId: "1:116670319510:web:de01c81e5c82870adcabb7"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
-  const db = getFirestore()
-
-  export {app, auth, db};
